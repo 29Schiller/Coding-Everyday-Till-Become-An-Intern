815453f6e5c5b6d03e84a3eee461f5b1c01bce3f
Tinntinn060104 <161331461+Tinntinn060104@users.noreply.github.com>
2024-05-19 22:08:05 +0700

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -1,30 +1,42 @@
 package Screnes;
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
-public class GameFrame extends JFrame{
-    public static final int SCREEN_WIDTH=1300;
-    public static final int SCREEN_HEIGHT=750;
+
+import Graphics.GamePanel;
+
+public class GameFrame extends JFrame {
+    public static final int SCREEN_WIDTH = 1300;
+    public static final int SCREEN_HEIGHT = 750;
     GamePanel gamePanel;
-    public GameFrame(){
-        Toolkit toolkit=this.getToolkit();
-        Dimension dimension=toolkit.getScreenSize();
-        this.setBounds((dimension.width-SCREEN_WIDTH)/2,(dimension.height-SCREEN_HEIGHT)/2,SCREEN_WIDTH,SCREEN_HEIGHT);
+    GameLoop gameLoop;
+
+    public GameFrame() {
+        Toolkit toolkit = this.getToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        this.setBounds((dimension.width - SCREEN_WIDTH) / 2, (dimension.height - SCREEN_HEIGHT) / 2, SCREEN_WIDTH, SCREEN_HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gamePanel=new GamePanel();
+        gamePanel = new GamePanel();
+        gameLoop = new GameLoop();
         add(gamePanel);
     }
+
     public static int getScreenWidth() {
         return SCREEN_WIDTH;
     }
+
     public static int getScreenHeight() {
         return SCREEN_HEIGHT;
     }
-    public void startGame(){
-        gamePanel.startgame();
+
+    public void startGame() {
+        Thread gameThread = new Thread(gameLoop);
+        gameThread.start();
     }
+
     public static void main(String[] args) {
-        GameFrame gameFrame=new GameFrame();
+        GameFrame gameFrame = new GameFrame();
         gameFrame.setVisible(true);
         gameFrame.startGame();
     }
--- GamePanel.java
+++ GamePanel.java
@@ -1,54 +1,19 @@
-package Screnes;
+package Graphics;
+
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Toolkit;
-public class GamePanel extends JPanel implements Runnable{
-    private Thread thread;
-    private Boolean isRunning;
-    private Toolkit t = Toolkit.getDefaultToolkit();
-    Background bg=new Background(this);
 
-    public GamePanel(){
+public class GamePanel extends JPanel {
+    Background bg = new Background(this);
+
+    public GamePanel() {
     }
 
-    //Draw
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         bg.render(g2);
-    }
-    public void startgame(){
-        if(thread==null){
-            isRunning=true;
-            thread=new Thread(this);
-            thread.start();
-        }
-    }
-
-    @Override
-    public void run(){
-        long FPS=60;
-        long Period=1000*1000000/FPS;//đơn vị nano second
-        long beginTime;
-        long sleepTime;
-        int a=0;
-        beginTime=System.nanoTime();
-        while(isRunning==true){
-            //update game
-            //render game
-            System.out.println("a="+(a++));
-            long deltaTime=System.nanoTime()-beginTime;
-            sleepTime=Period-deltaTime;
-            try{
-                if(sleepTime>0){
-                    Thread.sleep(sleepTime/1000000);}
-                else {
-                    Thread.sleep(Period/2000000);
-                }
-            }catch(InterruptedException ex){}
-            beginTime=System.nanoTime();
-        }
     }
 }
