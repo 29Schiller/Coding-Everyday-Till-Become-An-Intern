f95f0a515fd672d232115e990a89e56d9983d850
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2024-05-20 11:13:24 +0700

    Change

---
+++ PvZ/PvZ/Zombie.java
@@ -0,0 +1,51 @@
+package PvZ;
+
+public class Zombie {
+    private int health;
+    private int damage;
+    private int limit;
+    private long lastAttack;
+    private double attackWaitingTime;
+    private int lane;
+
+
+    public Zombie(int x, int y, int speed,int health, int damage, long lastAttack, double attackWaitingTime, int lane) {
+        super( x, y, speed);
+        this.health = health;
+        this.damage = damage;
+        this.lastAttack = 0;
+        limit=150;
+        this.attackWaitingTime = attackWaitingTime;
+        this.lane = lane;
+    }
+
+    @Override
+    public void act() {
+        if(x>limit){
+            setX(speed);
+        }
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public long getLastattack() {
+        return lastAttack;
+    }
+    
+    public void setLastattack(long lastattack) {
+        this.lastAttack = lastattack;
+    }
+
+    public double getAttackWaitingTime(){
+        return attackWaitingTime;
+    }
+    public void setAttackWaitingTime(double wai){
+        attackWaitingTime = wai;
+    }
+}
