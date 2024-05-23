06bb2008757c3e7c8bfe5d6579988beb1c6a75a9
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2024-05-23 15:00:29 +0700

    Update Exam1_HCMIU.java

--- gui/Exam1_HCMIU.java
+++ gui/Exam1_HCMIU.java
@@ -10,8 +10,8 @@ public class Exam1_HCMIU {
     private void importImg(){
         //buttonOfMenu = new Image[2];
         try {
-            buttonOfMenu[0] = t.getImage(getClass().getResource("/Scene/PLAY.png"));
-            buttonOfMenu[1] = t.getImage(getClass().getResource("/Scene/EXIT.png"));
+            buttonOfMenu[0] = t.getImage(getClass().getResource("res/pvz_logo.png"));
+            buttonOfMenu[1] = t.getImage(getClass().getResource("res/Button/PlayDemo.png"));
         }catch (Exception e){
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Error - importImage()");
@@ -24,13 +24,21 @@ public class Exam1_HCMIU {
     }
 
     public void render(Graphics g, Image img) {
-        g.drawImage(img,0,0, , , null);
+        Image background = t.getImage(getClass().getResource("res/wallpaper.jpg"));
+        g.drawImage(background,0,0, 1600, 750, null);
 //        drawButtons(g);
         importImg();
         drawImg(g);
     }
 
 	public static void main(String[] args) {
-		
+        JFrame windowFrame = new JFrame();
+        windowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowFrame.setSize(1600,750);
+        windowFrame.setVisible(true);
+        windowFrame.setLayout(null);
+        
+		JPanel picturPanel = new JPanel();
+        picturPanel.render();
 	}
 }
