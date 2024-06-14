0e603ebe36d39dbc9017087b3d397c09d3665924
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2024-06-15 00:14:34 +0700

    Delete src/Gui directory

--- src/Gui/GameScenes.java
+++
@@ -1,9 +0,0 @@
-package Gui;
-
-public enum GameScenes {
-    MENU, PLAYING, OVERGAME;
-    public static GameScenes gameScenes = GameScenes.MENU;
-    public static void setGameScenes(GameScenes scene) {
-        gameScenes = scene;
-    }
-}
